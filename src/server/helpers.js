78b57a875d3efbb96d7a048f6f89a6b5e260c34a
doomedboi <73707198+doomedboi@users.noreply.github.com>
2021-07-25 20:40:58 +1000

    SSID function changed

--- src/server/helpers.js
+++ src/server/helpers.js
@@ -4,12 +4,12 @@ module.exports = {
 
 function randomStr(len) {
     let result           = '';
-    const characters       = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const characters       = '123456789';
     const charactersLength = characters.length;
     for ( let i = 0; i < len; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-    return result;
+    return parseInt( result );
 }
 
 
