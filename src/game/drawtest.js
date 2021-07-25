7ce20680af89aae2480b936323b6f6c9d448fbff
GAGAVED <GAGAVED@DESKTOP-GH9OBCE>
2021-07-25 23:39:50 +1000

    g

--- src/game/drawtest.js
+++ src/game/drawtest.js
@@ -1,25 +1,35 @@
-//import { GameTable,EmptyEntity,Character,Structure,Player,Monster,Sprites} from "./classes";
+import {GameTable,Sprites} from "./classes.mjs";
 /*GLOBALS*/
-import { Sprites } from "./spritesLoader";
-//Sprites = require("./spritesLoader");
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-const CELLSIZE = 160;
+ctx.font = "30px serif";
 const FRAME = 10;
-const WIDTH = innerWidth;
-const HEIGHT = innerHeight;
 /*end globals*/
 /*canvas init*/
 canvas.height = 1000;
 canvas.width = 1000;
 
-
 Sprites.initial();
-//DESK = new GameTable('id1',6,6,Sprites.tableImg);
+let desk = new GameTable(93930492,6,6,Sprites.tableImg);
+desk.spawnPlayer(0,0);
+
+
+//document.addEventListener("mousedown", mouseDownHandler, false);
+document.addEventListener("mouseup", mouseUpHandler, false);
+
+function mouseUpHandler(e) {
+    if (e.button == 0 && e.target.id == 'myCanvas') {
+        let Xpos = e.offsetX;
+        let Ypos = e.offsetY;
+        //сдесь нужно будет передавать эти координаты геймконтроллеру, но его пока нет, так что обрабатываем грубо
+        let targetEntity = desk.getEntityByCoordinates(Xpos,Ypos);
+        desk.interact(desk.getPlayer1(),targetEntity);
+    }
+}
 function draw() {
     ctx.beginPath();
     ctx.clearRect(0, 0, canvas.width, canvas.height); // очищаем поверхность
-    //DESK.draw(ctx);
+    desk.draw(ctx);
     ctx.closePath();
 }
 setInterval(draw, FRAME); //задаем интервал главой функции.
