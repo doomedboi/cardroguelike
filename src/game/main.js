39b7b2e23614bc5f48c01fd6a156a428032fce1b
doomedboi <73707198+doomedboi@users.noreply.github.com>
2021-07-26 22:43:07 +1000

    update client-side
    fix bugs minor changes code beauty

--- src/game/main.js
+++ src/game/main.js
@@ -1,28 +1,23 @@
-
 import {GameTable,Sprites} from "./classes.mjs";
 /*GLOBALS*/
 let canvas, ctx, canvasId
 let playerNumber, roomExist = true
 let roomid = 0
-let desk, gameStart = false
+let gameStart = false
+let gameController
 /*end globals*/
 
-
 const socket = io('http://localhost:3000')
+
 /* listen to init event with handler */
 socket.on('init', handleInit)
-socket.on('gameState', handleGameStates)
 socket.on('gameOver', handleGameOver)
 socket.on('gameCode', handleGameCode)
 socket.on('invalidGameToken', handleInvalidGame)
 socket.on('tooManyPlayers', handleRoomIsFull)
 socket.on('mustMove', handleRequestMove)
 socket.on('generateGame', handleGenerateGame)
-
-function handleGenerateGame() {
-    initGame()
-}
-
+/* end of init list */
 
 /* init html elems */
 const startScreen = document.getElementById("startScreen")
@@ -33,90 +28,38 @@ const joinGameBtn = document.getElementById("joinGameBtn")
 const createdGameId = document.getElementById("createdGameId")
 /*end of init */
 
+/* listeners */
 newGameBtn.addEventListener('click', startNewGame)
 joinGameBtn.addEventListener('click', joinGame)
+/*end listeners */
 
-function init() {
-    startScreen.style.display = 'none'
-    gameScreen.style.display = "block"
-    canvas = document.getElementById("myCanvas")
-    ctx = canvas.getContext('2d')
-    ctx.font = "30px serif";
-    canvas.width = 1000
-    canvas.height = 1000
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
-    canvasId = setInterval(draw, 10); //задаем интервал главой функции.
+/* list of handlers */
+function handleGenerateGame() {
+    initGame()
 }
 
 function handleRequestMove(state) {
     state = JSON.parse(state)
-    console.log(state.f)
-    desk.interact(desk.getEntity(state.fx, state.fy), desk.getEntity(state.sx, state.sy))
-    //call interact(state)
+    gameController.desk.interact(gameController.desk.getEntity(state.fx, state.fy),gameController.desk.getEntity(state.sx, state.sy))
+    // switch id's to let make move other player
+    gameController.playerNumber = ( gameController.playerNumber + 1 ) % 2
 
-}
-
-//send to server msg to init both players game
-
-function initGame(row, col, ssid) {
-    Sprites.initial();
-    desk = new GameTable(93930491,6,6,Sprites.tableImg);
-    desk.spawnPlayer(0,0);
-    document.addEventListener("mouseup", mouseUpHandler, false);
-    gameStart = true
-}
-
-//TODO: call after my own move
-function requestMove(first, second) {
-    console.log('Im reqvesting move')
-    const roomId = codeInput.value
-    const obj1 = {fx: first.x,fy: first.y, sx: second.x, sy: second.y}
-    const obj = JSON.stringify(obj1)
-    console.log(obj)
-    socket.emit('makeMove', roomId, obj) //need pass two args
-}
-
-function startNewGame() {
-    //need to create new socket.io room
-    socket.emit('newGame')
-    init()
-}
-
-function clientInteract(player, dst) {
-    requestMove(desk.matrix[player.x][player.y], desk.matrix[dst.x][dst.y])
-}
-
-function joinGame() {
-    let state
-    const roomId = codeInput.value
-    socket.emit('joinRoom', roomId)
-    init()
-    console.log('send req from this local')
-    //send req to init game
-    socket.emit('generateGame', roomId)
-}
-
-function reset() {
-    gameScreen.style.display = 'none'
-    startScreen.style.display = 'block'
-    playerNumber = null
-    codeInput.value = ""
 }
 
 function handleInvalidGame() {
-    reset()
+    resetHtmlStates()
     alert("Invalid room token")
 }
 
 function handleRoomIsFull() {
-    reset()
+    resetHtmlStates()
     alert("Room is full")
 }
 
 function handleInit(playerId) {
     playerNumber = playerId
 }
-
+//replace to GM
 function handleGameOver() {
     alert("Looose")
 }
@@ -124,13 +67,8 @@ function handleGameOver() {
 function handleGameCode(code) {
     createdGameId.style.display = "block"
     alert(code)
+    codeInput.value = code
     createdGameId.innerText = code
-}
-
-//receive new game state from the server
-function handleGameStates(state) {
-    state = JSON.parse(state)
-    requestAnimationFrame(()=> GameDraw(state))
 }
 
 function handleEndOfGame(data) {
@@ -141,28 +79,99 @@ function handleEndOfGame(data) {
         alert("You lose")
     }
 }
+/*end of list */
 
-//function to draw every frame
-function GameDraw(){}
+function initCanvas() {
+    canvas = document.getElementById("myCanvas")
+    ctx = canvas.getContext('2d')
+    ctx.font = "30px serif";
+    canvas.width = 1000
+    canvas.height = 1000
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+}
 
+function prepaireBefGame() {
+    unhideMainPage()
+    initCanvas()
+    canvasId = setInterval(draw, 1000 / 10); //1 ms / frame count in sec = 1 frame interval
+}
+
+function unhideMainPage() {
+    startScreen.style.display = 'none'
+    gameScreen.style.display = "block"
+}
+
+
+/* init game when players are ready */
+function initGame(row, col, ssid) {
+    gameController = new GameController(playerNumber)
+    gameController.desk.spawnPlayer(0,0);
+    document.addEventListener("mouseup", mouseUpHandler, false);
+    gameStart = true
+
+    setInterval( ()=> {
+        console.log('ID:')
+        console.log(gameController.playerNumber)
+    }, 1000)
+}
+function startNewGame() {
+    //need to create new socket.io room
+    socket.emit('newGame')
+    prepaireBefGame()
+}
+function joinGame() {
+    const roomId = codeInput.value
+    socket.emit('joinRoom', roomId)
+    prepaireBefGame()
+    console.log('send req from this local')
+    //send req to init game
+    socket.emit('generateGame', roomId)
+}
+
+function resetHtmlStates() {
+    gameScreen.style.display = 'none'
+    startScreen.style.display = 'block'
+    playerNumber = null
+    codeInput.value = ""
+}
 
 function draw() {
     ctx.beginPath();
     ctx.clearRect(0, 0, canvas.width, canvas.height); // очищаем поверхность
     if (gameStart)
-        desk.draw(ctx);
+        gameController.desk.draw(ctx);
     ctx.closePath();
 }
 
 function mouseUpHandler(e) {
     if (e.button == 0 && e.target.id == 'myCanvas') {
-        let Xpos = e.offsetX;
-        let Ypos = e.offsetY;
-        //сдесь нужно будет передавать эти координаты геймконтроллеру, но его пока нет, так что обрабатываем грубо
-        let targetEntity = desk.getEntityByCoordinates(Xpos,Ypos);
-        clientInteract(desk.getPlayer1(),targetEntity)
-        //desk.interact(desk.getPlayer1(),targetEntity);
+        let xPos = e.offsetX;
+        let yPos = e.offsetY;
+        gameController.interact (xPos, yPos)
     }
 }
 
-//setInterval(draw, 10)
+class GameController {
+    constructor(playerNumber) {
+        this.playerNumber = playerNumber
+        Sprites.initial();
+        this.desk = new GameTable(93930491,6,6,Sprites.tableImg);
+    }
+
+    interact(xPos, yPos) {
+        let targetEntity = this.desk.getEntityByCoordinates(xPos,yPos);
+        if (this.playerNumber === 1 && this.desk.validMove(this.desk.getPlayer1(), targetEntity)) { //getPlayer(ид нужного игрока)
+            this.requestMove(this.desk.matrix[this.desk.getPlayer1().x][this.desk.getPlayer1().y], this.desk.matrix[targetEntity.x][targetEntity.y])
+        }
+    }
+
+    requestMove(first, second) {
+        const roomId = codeInput.value
+        console.log(roomId)
+        const obj1 = {fx: first.x, fy: first.y, sx: second.x, sy: second.y}
+        const obj = JSON.stringify(obj1)
+        console.log(obj)
+        socket.emit('makeMove', roomId, obj) //need pass two args
+    }
+
+}
